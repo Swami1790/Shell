872df6642a4cb415bbb67284d033379048175f85
Swami1790 <sandeep@pizoneinfotech.com>
2026-04-13 07:58:19 -0500

    config changes

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,28 @@
 import { Routes } from '@angular/router';
+import { loadRemoteModule } from '@angular-architects/module-federation';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  {
+    path: 'homepage',
+    loadChildren: () =>
+      loadRemoteModule({
+        type: 'module',
+        remoteEntry: 'http://localhost:4201/remoteEntry.js',
+        exposedModule: './Routes',
+      }).then(m => m.routes),
+  },
+  {
+  path: 'courses',
+  loadChildren: () =>
+    loadRemoteModule({
+      type: 'module',
+      remoteEntry: 'http://localhost:4202/remoteEntry.js',
+      exposedModule: './Routes',
+    }).then(m => m.routes),
+},
+  {
+    path: '',
+    redirectTo: 'homepage',
+    pathMatch: 'full',
+  },
+];
--- webpack.config.js
+++ webpack.config.js
@@ -2,10 +2,16 @@ const { shareAll, withModuleFederationPlugin } = require('@angular-architects/mo
 
 module.exports = withModuleFederationPlugin({
 
-  remotes: {
-    "mfe1": "http://localhost:3000/remoteEntry.js",    
+  /* remotes: {
+    "mfe1": "http://localhost:3000/remoteEntry.js",
   },
+ */
 
+ /*  remotes: {
+    homepage: "http://localhost:4201/remoteEntry.js",
+    courses: "http://localhost:4202/remoteEntry.js",
+  },
+ */
   shared: {
     ...shareAll({ singleton: true, strictVersion: true, requiredVersion: 'auto' }),
   },
